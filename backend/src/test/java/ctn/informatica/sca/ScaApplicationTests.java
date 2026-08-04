d2cb26245af56215209e9f4888748fdb8213c073
provingch <provingchill@gmail.com>
2026-08-03 21:59:07 -0300

    agrega esqueleto Spring Boot para backend nuevo

---
+++ backend/src/test/java/ctn/informatica/sca/ScaApplicationTests.java
@@ -0,0 +1,13 @@
+package ctn.informatica.sca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
